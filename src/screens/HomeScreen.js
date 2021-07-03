45681c2a7435ec4dcbe1817c790f0f05b79c1f5f
Umit Turk <uyt.turk@gmail.com>
2021-07-03 15:31:43 +0300

    responsive

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import "./HomeScreen.css";
 import { Fade } from "react-awesome-reveal";
 import { Link } from "react-router-dom";
 import Heading from "../components/Heading/Heading";
+import Featured from "../components/Featured/Featured";
+import Info from "../components/Info/Info";
 
 function HomeScreen() {
   return (
@@ -37,7 +39,63 @@ function HomeScreen() {
       </Fade>
 
       <Fade>
-          <Heading heading="TODAY IS YOURS" />
+        <Heading heading="TODAY IS YOURS" />
+      </Fade>
+
+      <div className="homeScreen__featured">
+        <Fade>
+          <Featured
+            title="NICELY NONDAIRY"
+            info="Say yes to sips of delight with our Honey Almondmilk Flat White and new Honey Almondmilk Cold Brew."
+            link="Explore the honey drinks"
+            path=""
+            image="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-68069.png"
+            background="#f0ebe0"
+            color="#1e3932"
+            className="featured__hoverLight"
+          />
+        </Fade>
+
+        <Fade>
+          <Featured
+            title="PISTACHIOOOOOOO"
+            info="Embrace creamy coffee bliss with our new Pistachio Latte and Pistachio Frappuccino® blended beverage."
+            link="Try the new pistachio drinks"
+            path=""
+            image="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-67826.png"
+            order="2"
+            background="#f0ebe0"
+            color="#1e3932"
+            className="featured__hoverLight"
+          />
+        </Fade>
+      </div>
+
+      <Fade>
+        <Heading heading="MORE TO DISCOVER" />
+      </Fade>
+
+      <Fade>
+      <div className="homeScreen__discover">
+          <Info
+            title="Order and pick up. Easy as that."
+            image="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-67828.jpg"
+            info="Just open the app, order your favorites, and enjoy contactless pay. From there, choose whichever pickup method is best for you."
+            link="See pickup options"
+            color="#1e3932"
+            background="#d4e9e4"
+            className="info__hoverLight"
+          />
+          <Info
+            title="Coffee delivered. Day made."
+            image="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-67829.jpg"
+            info="Make everything a little brighter. Enjoy a $0 Delivery Fee* on your first Uber Eats order over $15 through 1/31."
+            link="Order now"
+            color="#1e3932"
+            background="#d4e9e4"
+            className="info__hoverLight"
+          />
+        </div>
       </Fade>
     </div>
   );
